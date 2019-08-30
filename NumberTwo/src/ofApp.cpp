4de9f3e8a8ee0513d9a3fa64a30098b5b19907c7
Amay Kataria <amkatari@microsoft.com>
2019-08-30 00:35:41 -0500

    Number 2 project complete. A study into HSV colors, shadows and lights.

--- NumberTwo/src/ofApp.cpp
+++ NumberTwo/src/ofApp.cpp
@@ -1,71 +1,74 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-
+void ofApp::setup() {
+  ofBackground(ofColor::fromHsb(224, 255, 175));
+  ofEnableAntiAliasing();
+  ofEnableSmoothing();
+  
+  // Allocate FBO for drawing.
+  drawFbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
+  
+  cout << ofGetWidth() << ", " << ofGetHeight() << endl;
+  xGridSize = ofGetWidth()/16; yGridSize = (ofGetHeight()/16) + 1; 
+  cout << xGridSize << ", " << yGridSize << endl;
+  // Create grid.
+  for (int x=0; x < numRows; x++) {
+    for (int y=0; y < numColumns; y++) {
+      // Get the right hue color for each row and column
+      auto hue = x + y * numColumns;
+      glm::vec2 pos = glm::vec2(x*xGridSize, y*yGridSize);
+      auto cell = Cell(pos, hue);
+      grid.push_back(cell); 
+    }
+  }
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+  drawFbo.begin();
+    ofClear(0);
+    ofEnableAntiAliasing();
+//    ofEnableSmoothing();
+     // Draw the grid
+    for (auto c : grid) {
+      c.drawCellWithValueFlower(xGridSize, yGridSize);
+    }
+  drawFbo.end();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+  drawFbo.draw(0, 0);
+}
 
+// Nice experimental looking good shape.
+void ofApp::drawCircle() {
+  // Use this background for this
+  // ofBackground(ofColor::fromHsb(224, 255, 175));
+  int angleInc = 360/256;
+  int length = 1200;
+  ofPushMatrix();
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
+    int j = 0;
+    for (int a = 0; a < 360; a+= angleInc) {
+      ofPushMatrix();
+        ofRotateDeg(a);
+        ofSetColor(ofColor::fromHsb(240, 210, j));
+        ofDrawLine(0, 0, 0, length);
+      ofPopMatrix();
+      j++;
+    }
+  ofPopMatrix();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+  if (key == ' ') {
+    ofPixels p;
+    drawFbo.readToPixels(p);
+    auto fileName = ofToString(ofGetMinutes()) + ".png";
+    ofSaveImage(p, fileName, OF_IMAGE_QUALITY_BEST); 
+  }
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
--- NumberTwo/src/ofApp.h
+++ NumberTwo/src/ofApp.h
@@ -2,23 +2,122 @@
 
 #include "ofMain.h"
 
+class Cell {
+  public:
+    Cell(glm::vec2 pos, int h) {
+      position = pos;
+      hue = h;
+    }
+  
+    void drawCell(float xGridSize, float yGridSize) {
+      ofPushMatrix();
+        ofTranslate(position);
+        ofPushStyle();
+          ofSetColor(ofColor::fromHsb(hue, 255, 255));
+          ofDrawRectangle(0, 0, xGridSize, yGridSize);
+        ofPopStyle();
+      ofPopMatrix();
+    }
+  
+    void drawCellByValueVertical(float xGridSize, float yGridSize) {
+      // Subdivide the this cell into 256 parts and draw it with respect to the value
+      float interval = xGridSize/256; // Because there are 256 value intervals in a hue
+      ofPushMatrix();
+        ofTranslate(position);
+        int j = 0;
+        for (float i = 0.0; i < xGridSize; i += interval) {
+          auto color = ofColor::fromHsb(hue, 255, j);
+          ofPushStyle();
+            ofSetColor(color);
+            ofDrawLine(i, 0, i, yGridSize);
+          ofPopStyle();
+          j++;
+        }
+      ofPopMatrix();
+    }
+  
+    void drawCellByValueHorizontal(float xGridSize, float yGridSize) {
+      float interval = yGridSize/256; // Because there are 256 value intervals in a hue
+      ofPushMatrix();
+        ofTranslate(position);
+        int j = 0;
+        for (float i = 0; i < yGridSize; i += interval) {
+          auto color = ofColor::fromHsb(hue, 255, j);
+          ofPushStyle();
+            ofSetColor(color);
+            ofDrawLine(0, i, xGridSize, i);
+          ofPopStyle();
+          j++;
+        }
+      ofPopMatrix();
+    }
+  
+    void drawCellBySaturation(float xGridSize, float yGridSize) {
+      float interval = xGridSize/256; // Because there are 256 value intervals in a hue
+      ofPushMatrix();
+        ofTranslate(position);
+        int j = 0;
+        for (float i = 0; i < xGridSize; i += interval) {
+          auto color = ofColor::fromHsb(hue, j, 200);
+          ofPushStyle();
+            ofSetColor(color);
+            ofDrawLine(i, 0, i, yGridSize);
+          ofPopStyle();
+          j++;
+        }
+      ofPopMatrix();
+    }
+  
+    void drawCellWithValueFlower(float xGridSize, float yGridSize) {
+       ofPushMatrix();
+        ofTranslate(position);
+        // Draw the cell with original hue.
+        ofPushStyle();
+          ofSetColor(ofColor::fromHsb(hue, 255, 255));
+          ofDrawRectangle(0, 0, xGridSize, yGridSize);
+        ofPopStyle();
+        // Draw the value flower inside the cell.
+        float angleInterval = 360/32;
+        float length = yGridSize/2;
+        ofTranslate(xGridSize/2, yGridSize/2);
+        int j = 0;
+        for (float a = 0; a < 360; a+= angleInterval) {
+          ofPushMatrix();
+            ofRotateDeg(a);
+            ofPushStyle();
+            ofSetColor(ofColor::fromHsb(hue, 255, j));
+            ofDrawLine(0, 0, 0, length);
+            ofPopStyle();
+          ofPopMatrix();
+          j+=8;
+        }
+      ofPopMatrix();
+    }
+  
+    ofColor color;
+    glm::vec2 position;
+    int hue;
+}; 
+
 class ofApp : public ofBaseApp{
 
 	public:
 		void setup();
 		void update();
 		void draw();
+    void drawCircle();
 
 		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+  
+    int numRows = 16;
+    int numColumns = 16;
+    float xGridSize;
+    float yGridSize; 
+  
+    std::vector<Cell> grid;
+  
+    ofFbo drawFbo;
+  
+    // Number of Rows - 16
+    // Number of Columns - 16
 };
